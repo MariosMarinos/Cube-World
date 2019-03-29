132c26e3f54762f0cde137b7a90b65d4564de1d2
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-03-29 04:24:40 +0200

    Update Cube-World.py

--- Cube-World.py
+++ Cube-World.py
@@ -2,9 +2,10 @@
 def OpenFile() :
     fname = input("Enter file name: ")
     with open(fname) as fp:
-        for i, line in enumerate(fp):
-            if i >1: #2nd line
-                    
+        for i ,line in enumerate(fp):
+            if i>1 :
+                print (line)
 
 
 if __name__ == "__main__" :
+    OpenFile()
