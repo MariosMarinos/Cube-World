bc02c09fca17f0e60f93f31d595268ce7e0666e8
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-03-29 03:58:00 +0200

    Adding OpenFile

--- Cube-World.py
+++ Cube-World.py
@@ -1,3 +1,11 @@
 
+def OpenFile() :
+    fname = input("Enter file name: ")
+    try:
+        fh = open(fname)
+    except Exception as e:
+        print ('Sorry your file cannot be opened ')
+        quit()
+
 
 if __name__ == "__main__" :
