d10c45465786fbd8853510ba72f3eca3b35008ce
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-03-31 01:04:44 +0200

    return N

--- Cube-World.py
+++ Cube-World.py
@@ -1,11 +1,24 @@
 
+class Node():
+    def __init__ (self,list):
+        self.list = list()
+
 def OpenFile() :
+    File_list = list()
     fname = input("Enter file name: ")
     with open(fname) as fp:
         for i ,line in enumerate(fp):
             if i>1 :
-                print (line)
+                File_list.append(line)
+    return File_list
+
+def process_File() :
+    FileList = OpenFile()
+    x = FileList[0].split()
+    N = len(x)-2 #defines the number of cubes by splitting and
+                #leaving out the last and first element.
+    return N
 
 
 if __name__ == "__main__" :
-    OpenFile()
+    process_File()
